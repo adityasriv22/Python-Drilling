e4da4a0923b7932e869ca9bce37d7b4c26af6931
Aditya Srivastava <adiyash96@gmail.com>
2017-03-21 05:09:41 +0530

    binnary search

--- binnarysearch.py
+++ binnarysearch.py
@@ -1,15 +1,18 @@
 
-l=[2,3,5,55,5,1,11]
-key = len(l) // 2
+l=[2,3,5,55,6,1,11]
+l.sort()
+print l
+key = l[len(l) // 2]
+print key
 find = int(raw_input("Enter a number to find from the list"))
 if key == find:
 	print"Found the element"
-elif key > find:
+elif find < key:
 	for i in range(0,key):
 		if i == find:
 			print "Found by left"
-else key < find:
-	for i in range(key,len(l)):
+elif find > key:
+	for i in range(key,len(l)+1):
 		if i == find:
 			print "Found by right"
 
